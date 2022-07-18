604097a24b3e4b6685d74e55075113d6b894d944
raghavt20 <raghavt20@aospa.co>
2022-07-18 23:38:28 +0530

    spes: Move msm_irqbalance.conf to device tree
    Change-Id: Ibe73a58fddcfe143aba4ac361186e5785e5431a4

--- device.mk
+++ device.mk
@@ -274,6 +274,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
 
+# IRQ balance
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/gpio-keys.kl \
