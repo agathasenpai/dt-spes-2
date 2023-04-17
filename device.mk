79e098b93920f2f36a9b84f12e280bd659fceb39
Richard Raya <rdxzv.dev@gmail.com>
2023-04-17 00:30:12 -0300

    spes: perf: Import GameOptimizationFeature
    From sm6150 configs of LA.VENDOR.1.0.r1-16000-WAIPIO.QSSI13.0
    
    Change-Id: Ibf391c7244c1cc604925b4239b4dc3219bea5b0a

--- device.mk
+++ device.mk
@@ -422,7 +422,9 @@ PRODUCT_COPY_FILES += \
 # Perf
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml \
-    $(LOCAL_PATH)/configs/perf/perfconfigstore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfconfigstore.xml
+    $(LOCAL_PATH)/configs/perf/perfconfigstore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfconfigstore.xml \
+    $(LOCAL_PATH)/configs/perf/GameOptimizationFeature.xml:$(TARGET_COPY_OUT_VENDOR)/etc/lm/GameOptimizationFeature.xml
+
 
 # QMI
 PRODUCT_PACKAGES += \
