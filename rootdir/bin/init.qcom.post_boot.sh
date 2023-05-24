7b21026351ad36e61281b1360d20beab68307294
Ivan Martynov <johnivan19999@gmail.com>
2023-05-24 18:22:56 +0700

    spes: rootdir: Disable Input boost
    Change-Id: I987c93688de1fe073345a39ebef3b1e605fdc743

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -4143,8 +4143,8 @@ case "$target" in
             echo 1056000 > /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq
             echo 0 > /sys/devices/system/cpu/cpu4/cpufreq/schedutil/rtg_boost_freq
 
-            echo "0:1017600" > /sys/devices/system/cpu/cpu_boost/input_boost_freq
-            echo 80 > /sys/devices/system/cpu/cpu_boost/input_boost_ms
+            # echo "0:1017600" > /sys/devices/system/cpu/cpu_boost/input_boost_freq
+            # echo 80 > /sys/devices/system/cpu/cpu_boost/input_boost_ms
 
 	    echo 1 > /proc/sys/kernel/sched_walt_rotate_big_tasks
 
@@ -4256,8 +4256,8 @@ case "$target" in
             echo 1056000 > /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq
             echo 0 > /sys/devices/system/cpu/cpu4/cpufreq/schedutil/rtg_boost_freq
 
-            echo "0:1190000" > /sys/devices/system/cpu/cpu_boost/input_boost_freq
-            echo 120 > /sys/devices/system/cpu/cpu_boost/input_boost_ms
+            # echo "0:1190000" > /sys/devices/system/cpu/cpu_boost/input_boost_freq
+            # echo 120 > /sys/devices/system/cpu/cpu_boost/input_boost_ms
 
 	    echo 1 > /proc/sys/kernel/sched_walt_rotate_big_tasks
 
