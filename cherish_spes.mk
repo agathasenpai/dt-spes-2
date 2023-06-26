8dedd2f49929d586af1580cf96f0a27be0ff9645
Agatha q <naufalmarjan2014@gmail.com>
2023-06-26 12:39:36 +0000

    spes: CherishOS Official Bringup

--- cherish_spes.mk
+++ cherish_spes.mk
@@ -8,31 +8,37 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Nusantara stuff.
-$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+# Inherit some common Cherish stuff.
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
 # Inherit from spes device
 $(call inherit-product, device/xiaomi/spes/device.mk)
 
-# Nusantara stuff.
-NAD_BUILD_TYPE := OFFICIAL
+# CherishOS
+CHERISH_BUILD_TYPE=OFFICIAL
+
+# Maintainer
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.cherish.maintainer=Agatha
+
+# Rom Specific Flags
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_USES_BLUR := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_QUICK_TAP  := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_ENABLE_BLUR := true
 USE_PIXEL_CHARGING := true
 
-# GMS
-TARGET_GAPPS_ARCH := arm64
-USE_GAPPS := true
+# Gapps
+WITH_GMS := true
 
 # Product Specifics
-PRODUCT_NAME := nad_spes
+PRODUCT_NAME := cherish_spes
 PRODUCT_DEVICE := spes
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 11
 PRODUCT_MANUFACTURER := Xiaomi
-
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
