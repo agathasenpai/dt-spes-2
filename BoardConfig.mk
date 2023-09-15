763700355287721c9859d349998ccb2f7ea3836b
Zinadin Zidan <zidan44@pixelexperience.org>
2023-09-16 01:32:07 +0300

    spes: Update DT2W node
    * Access them via /sys/touchpanel/*
    
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: I3988d2167a1e86641061a685dabc200af32d1390

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -200,7 +200,7 @@ BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := bengal
 
 # Power
-TARGET_TAP_TO_WAKE_NODE := "/proc/tp_gesture"
+TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
 TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
 TARGET_USES_INTERACTION_BOOST := true
 
