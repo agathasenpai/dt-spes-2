742868e8cc8ba5b2ba2f16f07222c659c0b79b81
Agatha h <naufalmarjan2014@gmail.com>
2023-09-24 16:16:39 +0000

    spes: Fix typo clone kernel repo in vendorsetup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/mi-sdm680/android_kernel_xiaomi_sm6225 kernel/xiaomi/spes --depth1
+git clone https://github.com/mi-sdm680/android_kernel_xiaomi_sm6225 kernel/xiaomi/spes --depth=1
 git clone https://github.com/Agatha-Prjkt/device_xiaomi_spes-module device/xiaomi/spes-module
 git clone https://github.com/kenway214/vendor_bcr.git vendor/bcr
 git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
