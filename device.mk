1a404b5865caf67ada93f34994b6bc7dd6b6e7a0
Gabriel Biren <gbiren@google.com>
2023-10-18 18:47:38 -0300

    spes: Update AIDL Wifi Vendor HAL.
    Bug: 274964641
    Test: Pre-submit tests
    Change-Id: I4d9114377fe75b593bbfe6a59f03d8f5d607d6a1

--- device.mk
+++ device.mk
@@ -579,7 +579,7 @@ PRODUCT_PACKAGES += \
 
 # WiFi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi-service \
     android.hardware.wifi.hostapd \
     android.hardware.wifi.supplicant \
     hostapd \
